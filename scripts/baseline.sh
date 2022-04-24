0c021cd26b29e5ed83bfc2489b4d514544e09178
Andrei <inwaves@live.com>
2022-04-24 09:10:29 +0100

    Prepare all baseline tests

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -30,4 +30,17 @@
 #SBATCH -p ampere
 #/bin/bash
 
-python main.py --configs configs/code2/gnn-transformer/no-virtual/pooling=cls+norm_input.yml 
+python main.py --configs configs/NCI1/gcn/base.yml
+python main.py --configs configs/NCI1/gcn-virtual/base.yml
+python main.py --configs configs/NCI1/gnn-transformer/gd=128+gdp=0.1+tdp=0.1+l=3+cosine.yml
+python main.py --configs configs/NCI1/gnn-transformer/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
+python main.py --configs configs/NCI1/transformer-gnn/no-virtual/gd=128+gdp=0.1+tdp=0.1+l=3+cosine.yml
+python main.py --configs configs/NCI1/transformer-gnn/no-virtual/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
+
+python main.py --configs configs/NCI109/gcn/base.yml
+python main.py --configs configs/NCI109/gcn-virtual/base.yml
+python main.py --configs configs/NCI109/gnn-transformer/gd=128+gdp=0.1+tdp=0.1+l=3+cosine.yml
+python main.py --configs configs/NCI109/gnn-transformer/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
+python main.py --configs configs/NCI109/transformer-gnn/no-virtual/gd=128+gdp=0.1+tdp=0.1+l=3+cosine.yml
+python main.py --configs configs/NCI109/transformer-gnn/no-virtual/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
+
