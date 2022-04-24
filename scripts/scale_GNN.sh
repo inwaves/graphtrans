2f7dde5a12b81e099e62e1f15e70df2e3424b4e1
Andrei <inwaves@live.com>
2022-04-24 13:06:21 +0100

    Remove GT scale, fix GNN scale

--- scripts/scale_GNN.sh
+++ scripts/scale_GNN.sh
@@ -31,9 +31,9 @@
 #/bin/bash
 
 python main.py --configs configs/NCI1/gcn/scaled_layers_constant_embedding.yml
-python main.py --configs configs/NCI1/gcn/scaled_embedding_constant_layers.yml
-python main.py --configs configs/NCI1/gcn/scaled_layers_scaled_embedding.yml
-
-python main.py --configs configs/NCI109/gcn/scaled_layers_constant_embedding.yml
-python main.py --configs configs/NCI109/gcn/scaled_embedding_constant_layers.yml
-python main.py --configs configs/NCI109/gcn/scaled_layers_scaled_embedding.yml
+#python main.py --configs configs/NCI1/gcn/scaled_embedding_constant_layers.yml
+#python main.py --configs configs/NCI1/gcn/scaled_layers_scaled_embedding.yml
+#
+#python main.py --configs configs/NCI109/gcn/scaled_layers_constant_embedding.yml
+#python main.py --configs configs/NCI109/gcn/scaled_embedding_constant_layers.yml
+#python main.py --configs configs/NCI109/gcn/scaled_layers_scaled_embedding.yml
