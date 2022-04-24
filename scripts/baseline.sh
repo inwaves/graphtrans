78a7a3e7b1e56e951fdf44360f3de24da368eb4a
Andrei <inwaves@live.com>
2022-04-24 11:32:43 +0100

    More experiment configs

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -36,6 +36,8 @@ python main.py --configs configs/NCI1/gnn-transformer/no-virtual/gd=128+gdp=0.1+
 python main.py --configs configs/NCI1/gnn-transformer/no-virtual/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
 python main.py --configs configs/NCI1/transformer-gnn/no-virtual/gd=128+gdp=0.1+tdp=0.1+l=3+cosine.yml
 python main.py --configs configs/NCI1/transformer-gnn/no-virtual/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
+python main.py --configs configs/NCI1/transformer/pooling=cls.yml
+
 
 python main.py --configs configs/NCI109/gcn/base.yml
 python main.py --configs configs/NCI109/gcn-virtual/base.yml
@@ -43,4 +45,5 @@ python main.py --configs configs/NCI109/gnn-transformer/no-virtual/gd=128+gdp=0.
 python main.py --configs configs/NCI109/gnn-transformer/no-virtual/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
 python main.py --configs configs/NCI109/transformer-gnn/no-virtual/gd=128+gdp=0.1+tdp=0.1+l=3+cosine.yml
 python main.py --configs configs/NCI109/transformer-gnn/no-virtual/gin+gdp=0.1+tdp=0.1+l=4+cosine.yml
+python main.py --configs configs/NCI109/transformer/pooling=cls.yml
 
